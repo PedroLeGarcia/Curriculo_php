1129d60d5f570bafc1037a6d68e8a51a4c38cd6d
PedroLeGarcia <91338154+PedroLeGarcia@users.noreply.github.com>
2022-03-25 15:51:33 -0300

    Update curriculo.php
    ARRUMEI

--- curriculo.php
+++ curriculo.php
@@ -1,4 +1,4 @@
-        <?php
+<?php
     if($_POST){
         $nome = $_POST['nome'];
         $rg = $_POST['rg'];
@@ -15,21 +15,22 @@
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
                 <!--Bootstrap link-->
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+                
                 <title>Currículo.php</title>
+                <link rel="stylesheet" href="./curriculo.css">
             </head>
             <body>
-                <div>
-                    <h3>Nome: '.$nome.'</h3>
-                    <h3>RG: '.$rg.'</h3>
-                    <h3>CPF: '.$cpf.'</h3>
-                    <h3>Idade: '.$data.'</h3>
-                    <h3>Email: '.$email.'</h3>
-                    <h3>Contato: '.$cont.'</h3>
-                    <h3>Endereço: '.$exp.'</h3>
+                <div class="container" id="curriculo">
+                     <h1> Seu Currículo </h1>
+                    <h3>Nome: '.$nome.'</h3> <br>
+                    <h3>RG: '.$rg.'</h3> <br>
+                    <h3>CPF: '.$cpf.'</h3> <br>
+                    <h3>Idade: '.$data.'</h3> <br>
+                    <h3>Email: '.$email.'</h3> <br>
+                    <h3>Contato: '.$contanto.'</h3> <br>
+                    <h3>Experiencia: '.$exp.'</h3> <br>
                 </div>    
             </body>
             </html>';
@@ -39,5 +40,6 @@
         fwrite($arquivo, $html);
         fclose($arquivo);
     }
+    header('location:'.$site);
 
     ?>
